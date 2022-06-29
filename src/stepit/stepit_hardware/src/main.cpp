8717e756398289adaea8851cdc845a62274560c4
g.remigi <g.remigi@kineticsystem.org>
2022-06-29 12:02:55 +0200

    Removed incorrect files from build.

--- src/stepit/stepit_hardware/src/main.cpp
+++ src/stepit/stepit_hardware/src/main.cpp
@@ -1,10 +1,25 @@
 #include "serial/serial.h"
 #include <cstdlib>
 #include <iostream>
+#include <vector>
+
+void enumerate_ports() {
+  std::vector<serial::PortInfo> devices_found = serial::list_ports();
+
+  std::vector<serial::PortInfo>::iterator iter = devices_found.begin();
+
+  while (iter != devices_found.end()) {
+    serial::PortInfo device = *iter++;
+
+    printf("(%s, %s, %s)\n", device.port.c_str(), device.description.c_str(),
+           device.hardware_id.c_str());
+  }
+}
 
 int main(int argc, char *argv[]) {
 
-  std::cout << "Hello world" << std::endl;
+  enumerate_ports();
+  std::cout << "Hello worldXXX!" << std::endl;
 
   return EXIT_SUCCESS;
 }
