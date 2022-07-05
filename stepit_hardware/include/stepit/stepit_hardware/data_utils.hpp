079146d45da494712b76b5cea093ebad3bb22709
Giovanni Remigi <g.remigi@kineticsystem.org>
2022-07-05 11:47:54 +0200

    Added tests for data_utils.

--- stepit_hardware/include/stepit/stepit_hardware/data_utils.hpp
+++ stepit_hardware/include/stepit/stepit_hardware/data_utils.hpp
@@ -22,40 +22,49 @@
 
 #include <array>
 #include <string>
-#include <vector>
 
 /**
  * Utility class to convert standard types into sequence of bytes and back.
+ * Please not that Linux on X86 and Rasperry PI are Little-Endian systems.
+ * It is but convention to send data to the network in Big-Endian order.
+ * To find out if a system is Little or Big-Endian, type the following command
+ * in a terminal.
+ *
+ * lscpu | grep Endian
  */
 namespace stepit::stepit_hardware::data_utils
 {
+constexpr std::array<char, 16> vChars = {
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+};
+
 /**
  * Convert a 32 bits integer into a 4 bytes array in Big Endian byte order.
  * @param value A 32 bits integer to convert.
  * @return The resulting 4 bytes array in Big Endian byte order.
  */
-std::array<uint8_t, 4> from_int32(const int32_t value);
+std::array<uint8_t, 4> from_int_32(const int32_t value);
 
 /**
  * Convert a 4 bytes array in Big Endian byte order into a 32 bits integer.
  * @param bytes A 4 bytes array in Big Endian byte order.
  * @return The resuting 32 bits integer.
  */
-int32_t to_int32(const std::array<uint8_t, 4>& bytes);
+int32_t to_int_32(const std::array<uint8_t, 4>& bytes);
 
 /**
  * Convert a 16 bits integer into an 2 bytes array in Big Endian byte order.
  * @param value A 16 bits integer to convert.
  * @return The resulting 2 bytes array in Big Endian byte order.
  */
-std::array<uint8_t, 2> from_int16(const int16_t value);
+std::array<uint8_t, 2> from_int_16(const int16_t value);
 
 /**
  * Convert a 2 bytes array in Big Endian byte order into a 16 bits integer.
  * @param bytes A 2 bytes array in Big Endian byte order.
  * @return The resuting 16 bits integer.
  */
-int16_t to_int16(const std::array<uint8_t, 2>& bytes);
+int16_t to_int_16(const std::array<uint8_t, 2>& bytes);
 
 std::array<uint8_t, 4> from_float(const float value);
 
@@ -66,5 +75,24 @@ float to_float(const std::array<uint8_t, 4>& bytes);
  * @param bytes The sequence of bytes.
  * @return A string containing the sequence of hex numbers.
  */
-std::string to_hex(const std::vector<uint8_t>& bytes);
+template <std::size_t SIZE>
+std::string to_hex(const std::array<uint8_t, SIZE>& bytes)
+{
+  std::string hex;
+  for (auto it = std::begin(bytes); it != std::end(bytes); ++it)
+  {
+    if (it != bytes.begin())
+    {
+      hex += " ";
+    }
+    uint8_t ch = *it;
+    char ls = vChars[(ch & 0xF)];
+    char ms = vChars[((ch >> 4) & 0xF)];
+    hex += "0x";
+    hex += ms;
+    hex += ls;
+  }
+
+  return hex;
+}
 }  // namespace stepit::stepit_hardware::data_utils
--- stepit_hardware/src/data_utils.cpp
+++ stepit_hardware/src/data_utils.cpp
@@ -20,13 +20,9 @@
 
 #include <stepit/stepit_hardware/data_utils.hpp>
 
-constexpr std::array<char, 16> vChars = {
-  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-};
-
 namespace stepit::stepit_hardware::data_utils
 {
-std::array<uint8_t, 4> from_int32(const int32_t value)
+std::array<uint8_t, 4> from_int_32(const int32_t value)
 {
   std::array<uint8_t, 4> bytes;
   bytes[0] = (value >> 24) & 0xFF;
@@ -36,13 +32,13 @@ std::array<uint8_t, 4> from_int32(const int32_t value)
   return bytes;
 }
 
-int32_t to_int32(const std::array<uint8_t, 4>& bytes)
+int32_t to_int_32(const std::array<uint8_t, 4>& bytes)
 {
   int32_t value = (bytes[0] << 24) + (bytes[1] << 16) + (bytes[2] << 8) + bytes[3];
   return value;
 }
 
-std::array<uint8_t, 2> from_int16(const int16_t value)
+std::array<uint8_t, 2> from_int_16(const int16_t value)
 {
   std::array<uint8_t, 2> bytes;
   bytes[0] = (value >> 8) & 0xFF;
@@ -50,7 +46,7 @@ std::array<uint8_t, 2> from_int16(const int16_t value)
   return bytes;
 }
 
-int16_t to_int16(const std::array<uint8_t, 2>& bytes)
+int16_t to_int_16(const std::array<uint8_t, 2>& bytes)
 {
   int16_t value = (bytes[0] << 8) + bytes[1];
   return value;
@@ -58,32 +54,24 @@ int16_t to_int16(const std::array<uint8_t, 2>& bytes)
 
 std::array<uint8_t, 4> from_float(const float value)
 {
-  return {};
+  const uint8_t* p = reinterpret_cast<const uint8_t*>(&value);
+  std::array<uint8_t, 4> bytes;
+  bytes[0] = p[3];
+  bytes[1] = p[2];
+  bytes[2] = p[1];
+  bytes[3] = p[0];
+  return bytes;
 }
 
 float to_float(const std::array<uint8_t, 4>& bytes)
 {
-  return 0.0;
-}
-
-std::string to_hex(const std::vector<uint8_t>& bytes)
-{
-  std::string hex;
-  for (auto it = std::begin(bytes); it != std::end(bytes); ++it)
-  {
-    if (it != bytes.begin())
-    {
-      hex += " ";
-    }
-    uint8_t ch = *it;
-    char ls = vChars[(ch & 0xF)];
-    char ms = vChars[((ch >> 4) & 0xF)];
-    hex += "0x";
-    hex += ms;
-    hex += ls;
-  }
-
-  return hex;
+  float value;
+  uint8_t* p = reinterpret_cast<uint8_t*>(&value);
+  p[0] = bytes[3];
+  p[1] = bytes[2];
+  p[2] = bytes[1];
+  p[3] = bytes[0];
+  return value;
 }
 
 }  // namespace stepit::stepit_hardware::data_utils
