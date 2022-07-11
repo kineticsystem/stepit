8d45e86f8f7b45746e4a357d853e9744da63efaa
Giovanni Remigi <g.remigi@kineticsystem.org>
2022-07-11 13:56:42 +0200

    Implemented command_interface_test.

--- stepit_hardware/test/command_interface_test.cpp
+++ stepit_hardware/test/command_interface_test.cpp
@@ -20,6 +20,8 @@
 
 #include <gmock/gmock.h>
 #include <stepit_hardware/request.h>
+#include <stepit_hardware/motor_move_to_command.h>
+#include <stepit_hardware/command_interface.h>
 #include <stepit_hardware/serial_interface.h>
 #include <mock_serial_interface.h>
 
@@ -28,6 +30,25 @@ namespace stepit_hardware::tests
 TEST(command_interface, send)
 {
   MockSerialInterface mock;
+  CommandInterface cmd_interface{ &mock };
+
+  EXPECT_CALL(mock, write(std::vector<uint8_t>{ {
+                        0x7e,  // Delimiter
+                        0x00,  // Request ID
+                        0x71,  // Motor Move To command ID
+                        0x01,  // Motor ID
+                        0x00,  // Position (MSB)
+                        0x00,  // Position
+                        0x0b,  // Position
+                        0xb8,  // Position (LSB)
+                        0x05,  // CRC (MSB)
+                        0x17,  // CRC (LSB)
+                        0x7e   // Delimiter
+                    } }));
+
+  MotorMoveToCommand cmd{ 1, 3000 };
+  cmd_interface.send(cmd);
+
 }  // namespace stepit_hardware::tests
 
 }  // namespace stepit_hardware::tests
