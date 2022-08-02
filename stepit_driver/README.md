63c447c4aee2d3052d35b2a8e5c4bfb982365bb3
Giovanni Remigi <g.remigi@kineticsystem.org>
2022-08-02 09:10:14 +0200

    Logitech driver.

---
+++ stepit_driver/README.md
@@ -0,0 +1,11 @@
+# StepIt Joystick driver
+
+This package starts up a ROS node to manage a Joystick teleoperation.
+
+## Configuration
+
+One easy way to check if you joystick works with Ubuntu is to install the package jstest-gtk.
+
+`sudo apt install jstest-gtk`
+
+<img src="docs/images/jstest-gtk.png" width="60%">
---
+++ stepit_driver/launch/teleop.launch.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022, Giovanni Remigi
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+
+import launch
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    joy_config = LaunchConfiguration("joy_config")
+    joy_dev = LaunchConfiguration("joy_dev")
+    config_filepath = LaunchConfiguration("config_filepath")
+
+    return launch.LaunchDescription(
+        [
+            DeclareLaunchArgument("joy_vel", default_value="stepit/cmd_vel"),
+            DeclareLaunchArgument("joy_config", default_value="logitech"),
+            DeclareLaunchArgument("joy_dev", default_value="/dev/input/js0"),
+            DeclareLaunchArgument(
+                "config_filepath",
+                default_value=[  # ../stepit_driver/config/logitech.config.yaml
+                    TextSubstitution(
+                        text=os.path.join(
+                            get_package_share_directory("stepit_driver"), "config", ""
+                        )
+                    ),
+                    joy_config,
+                    TextSubstitution(text=".config.yaml"),
+                ],
+            ),
+            Node(
+                package="joy",
+                executable="joy_node",
+                name="joy_node",
+                parameters=[
+                    {
+                        "dev": joy_dev,
+                        "deadzone": 0.3,
+                        "autorepeat_rate": 20.0,
+                    }
+                ],
+            ),
+            Node(
+                package="teleop_twist_joy",
+                executable="teleop_node",
+                name="teleop_twist_joy_node",
+                parameters=[config_filepath],
+                remappings={("/cmd_vel", LaunchConfiguration("joy_vel"))},
+            ),
+        ]
+    )
