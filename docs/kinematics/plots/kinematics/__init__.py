a4bf0477c9478570b9ce94f8c14cc4fb146ee747
Giovanni Remigi <g.remigi@kineticsystem.org>
2022-08-19 11:35:23 +0200

    Python kinematics formulas.

---
+++ docs/kinematics/plots/kinematics/__init__.py
@@ -0,0 +1,3 @@
+from .kinematics import *
+
+__all__ = kinematics.__all__
--- docs/kinematics/plots/main.py
+++ docs/kinematics/plots/main.py
@@ -27,8 +27,9 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import kinematics
+
 import matplotlib.pyplot as plt
-import math
 import numpy as np
 
 # This is a library that has been used to figure out trajectory profiles of
@@ -37,204 +38,17 @@ import numpy as np
 # Trajectory profiles are then plotted to validate the result.
 
 
-def sgn(a):
-    return bool(a > 0) - bool(a < 0)
-
-
-def time_to_stop(a: float, v0: float):
-    """
-    Return the time to stop for a motor with acceleration a and rotating speed v0.
-    :param a:  The motor acceleration.
-    :param v0: The current speed.
-    :return:   The time for the motor to stop.
-    """
-    return abs(v0 / a)
-
-
-def distance_to_stop(a: float, v0: float):
-    """
-    Return the absolute distance to stop for a motor with acceleration a and rotating speed v0.
-    :param a:  The motor acceleration.
-    :param v0: The current speed.
-    :return:   The absolute distance for the motor to stop.
-    """
-    return 0.5 * pow(v0, 2) / a
-
-
-def time_to_go_0(v_max: float, a: float, x0: float, x1: float):
-    """
-    Return the time for a motor with acceleration a and zero initial velocity to move from position x0 to position x1.
-    :param v_max: The maximum velocity.
-    :param a:     The acceleration.
-    :param x0:    The initial position.
-    :param x1:    The final position.
-    :return:      The motor velocity.
-    """
-    dx = x1 - x0
-    total_time = 0
-    if abs(dx) >= pow(v_max, 2) / a:
-        t1 = v_max / a
-        t2 = t1 + max(0, (abs(dx) - pow(v_max, 2) / a)) / v_max
-        total_time = t1 + t2
-    elif abs(dx) >= 0:
-        t1 = math.sqrt(abs(dx) / a)
-        total_time = 2 * t1
-    return total_time
-
-
-def time_to_go(v_max: float, a: float, v0: float, x0: float, x1: float):
-    dx = x1 - x0
-    d_stop = distance_to_stop(a, v0)
-    t_stop = time_to_stop(a, v0)
-    total_time = 0
-    if (v0 >= 0 and dx >= d_stop) or (v0 <= 0 and dx <= -d_stop):  # Keep accelerating.
-        total_time = time_to_go_0(v_max, a, x0, x1 + sgn(v0) * d_stop) - t_stop
-    elif abs(dx) >= 0:  # Decelerate and reverse.
-        total_time = time_to_go_0(v_max, a, x0, x1 - sgn(v0) * d_stop) + t_stop
-    return total_time
-
-
 def position_v(v_max: float, a: float, v0: float, x0: float, x1: float, t: np.ndarray):
     x = [0.0] * len(t)
     for i in range(0, len(t)):
-        x[i] = position(v_max, a, v0, x0, x1, t[i])
+        x[i] = kinematics.position(v_max, a, v0, x0, x1, t[i])
     return x
-
-
-def position_0(v_max: float, a: float, x0: float, x1: float, t: float):
-    """
-    Velocity at time t of a motor with acceleration a and zero initial velocity, moving from position x0 to position x1.
-    :param v_max: The maximum velocity.
-    :param a:     The acceleration.
-    :param x0:    The initial position.
-    :param x1:    The final position.
-    :param t:     The time.
-    :return:      The motor position.
-    """
-    dx = x1 - x0
-    x = 0
-    if abs(dx) >= pow(v_max, 2) / a:
-        t1 = v_max / a
-        t2 = t1 + max(0, (abs(dx) - pow(v_max, 2) / a)) / v_max
-        t3 = t1 + t2
-        if t <= t1:  # v = a * t
-            x = 0.5 * a * pow(t, 2)
-        elif t <= t2:  # v = a * t1
-            x = 0.5 * pow(v_max, 2) / a + v_max * (t - t1)
-        elif t <= t3:  # v = a * t1 - a * (t - t2)
-            x = 0.5 * pow(v_max, 2) / a + v_max * (t - t1) - 0.5 * a * pow(t - t2, 2)
-        else:
-            x = pow(v_max, 2) / a + v_max * (t2 - t1)
-    elif abs(dx) >= 0:
-        t1 = math.sqrt(abs(dx) / a)
-        t2 = 2 * t1
-        if t <= t1:
-            x = 0.5 * a * pow(t, 2)
-        elif t <= t2:
-            x = 0.5 * abs(dx) + a * t1 * (t - t1) - 0.5 * a * pow(t - t1, 2)
-        else:
-            x = abs(dx)
-    return x * sgn(dx)
-
-
-def position(v_max: float, a: float, v0: float, x0: float, x1: float, t: float):
-    """
-    Velocity at time t of a motor with acceleration a and initial velocity v0, moving from position x0 to position x1.
-    :param v_max: The maximum velocity.
-    :param a:     The acceleration.
-    :param v0:    The initial velocity.
-    :param x0:    The initial position.
-    :param x1:    The final position.
-    :param t:     The time.
-    :return:      The motor position.
-    """
-
-    dx = x1 - x0
-    d_stop = distance_to_stop(a, v0)
-    t_stop = time_to_stop(a, v0)
-    if (v0 >= 0 and dx >= d_stop) or (v0 <= 0 and dx <= -d_stop):  # Keep accelerating.
-        x = (
-            position_0(v_max, a, x0, x1 + sgn(v0) * d_stop, t + t_stop)
-            - sgn(v0) * d_stop
-        )
-    else:
-        if t <= t_stop:  # Decelerate.
-            x = v0 * (t - t_stop) + sgn(v0) * (
-                0.5 * a * (pow(t_stop, 2) - pow(t, 2)) + d_stop
-            )
-        else:  # Reverse.
-            x = (
-                position_0(v_max, a, x0, x1 - sgn(v0) * d_stop, t - t_stop)
-                + sgn(v0) * d_stop
-            )
-    return x + x0
 
 
 def velocity_v(v_max: float, a: float, v0: float, x0: float, x1: float, t: np.ndarray):
     v = [0.0] * len(t)
     for i in range(0, len(t)):
-        v[i] = velocity(v_max, a, v0, x0, x1, t[i])
-    return v
-
-
-def velocity_0(v_max: float, a: float, x0: float, x1: float, t: float):
-    """
-    Velocity at time t of a motor with acceleration a and zero initial velocity, moving from position x0 to position x1.
-    :param v_max: The maximum velocity.
-    :param a:     The acceleration.
-    :param x0:    The initial position.
-    :param x1:    The final position.
-    :param t:     The time.
-    :return:      The motor velocity.
-    """
-    dx = x1 - x0
-    v = 0
-    if abs(dx) >= pow(v_max, 2) / a:
-        t1 = v_max / a
-        t2 = t1 + max(0, (abs(dx) - pow(v_max, 2) / a)) / v_max
-        t3 = t1 + t2
-        if t <= t1:
-            v = a * t
-        elif t <= t2:
-            v = a * t1
-        elif t <= t3:
-            v = a * t1 - a * (t - t2)
-        else:
-            v = 0
-    elif abs(dx) >= 0:
-        t1 = math.sqrt(abs(dx) / a)
-        t2 = 2 * t1
-        if t <= t1:
-            v = a * t
-        elif t <= t2:
-            v = a * t1 - a * (t - t1)
-        else:
-            v = 0
-    return v * sgn(dx)
-
-
-def velocity(v_max: float, a: float, v0: float, x0: float, x1: float, t: float):
-    """
-    Velocity at time t of a motor with acceleration a and initial velocity v0, moving from position x0 to position x1.
-    :param v_max: The maximum velocity.
-    :param a:     The acceleration.
-    :param v0:    The initial velocity.
-    :param x0:    The initial position.
-    :param x1:    The final position.
-    :param t:     The time.
-    :return:      The motor velocity.
-    """
-
-    dx = x1 - x0
-    d_stop = distance_to_stop(a, v0)
-    t_stop = time_to_stop(a, v0)
-    if (v0 >= 0 and dx >= d_stop) or (v0 <= 0 and dx <= -d_stop):  # Keep accelerating.
-        v = velocity_0(v_max, a, x0, x1 + sgn(v0) * d_stop, t + t_stop)
-    else:
-        if t <= t_stop:  # Decelerate.
-            v = v0 - sgn(v0) * a * t
-        else:  # Reverse.
-            v = velocity_0(v_max, a, x0, x1 - sgn(v0) * d_stop, t - t_stop)
+        v[i] = kinematics.velocity(v_max, a, v0, x0, x1, t[i])
     return v
 
 
@@ -247,7 +61,7 @@ def plot():
     v_max = 1
     x0 = 0
     x1 = 0
-    t_max = time_to_go(v_max, a, -v_max, x0, x0 + x_max)
+    t_max = kinematics.time_to_go(v_max, a, -v_max, x0, x0 + x_max)
 
     t = np.linspace(0, t_max, 200)
     fig, ax = plt.subplots(figsize=(12, 6), dpi=200)
@@ -358,7 +172,7 @@ def plot():
             )
         )
 
-        t_max = time_to_go(
+        t_max = kinematics.time_to_go(
             v_max=v_max,
             a=a_slider.val,
             v0=v0_slider.val,
