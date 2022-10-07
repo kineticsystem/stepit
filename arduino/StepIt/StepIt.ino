d35b0be5a1bdcd2c5eea317808dd445d5a35cbe1
Giovanni Remigi <g.remigi@kineticsystem.org>
2022-10-07 23:03:00 +0200

    Arduino now working with position and velocity commands.

--- arduino/StepIt/StepIt.ino
+++ arduino/StepIt/StepIt.ino
@@ -30,43 +30,43 @@
 
 // Motors connections.
 
-static const byte STEPPER0_STEP_PIN = 9;
-static const byte STEPPER0_DIR_PIN = 10;
-static const byte STEPPER1_STEP_PIN = 11;
-static const byte STEPPER1_DIR_PIN = 12;
+constexpr byte STEPPER0_STEP_PIN = 9;
+constexpr byte STEPPER0_DIR_PIN = 10;
+constexpr byte STEPPER1_STEP_PIN = 11;
+constexpr byte STEPPER1_DIR_PIN = 12;
+
+// Number of full steps to achieve a rotation: 200 * 8 µ-steps
+constexpr long TOTAL_STEPS = 1600;
 
 // This is the information sent by Arduino during the connection handshake.
-const char NAME[] = "STEPPIT\0";
+constexpr char NAME[] = "STEPPIT\0";
 
 // The time between each execution of the run method.
-const int INTERRUPT_TIME_US = 90;
+constexpr int INTERRUPT_TIME_US = 90;
 
 // If no command is received within this time, motors are stopped and the
 // command buffer cleaned.
-const int TIMEOUT_MS = 1000;
+constexpr int TIMEOUT_MS = 1000;
 
 // For each command received, Arduino returns a response with success or error
 // code and possibly some data.
 
-byte MOVE_CMD = 0x70;               // Move motors a given amount of steps.
-byte MOVE_TO_CMD = 0x71;            // Move motors to a given position.
-byte STOP_CMD = 0x72;               // Stop motors.
-byte STOP_ALL_CMD = 0x74;           // Stop all motors.
-byte STATUS_CMD = 0x75;             // Request motors status, position and speed.
-byte INFO_CMD = 0x76;               // Request controller info for connection handshaking.
-byte SET_SPEED_CMD = 0x77;          // Set motors target relative speed (0-100).
-byte SET_MOTORS_ENABLED_CMD = 0x7A; // Enable interrupt to control the motors.
+constexpr byte MOVE_CMD = 0x71;                // Move motors at maximum speed to a given position.
+constexpr byte SPEED_CMD = 0x77;               // MOve motors at the given speed.
+constexpr byte STATUS_CMD = 0x75;              // Request motors status, position and speed.
+constexpr byte INFO_CMD = 0x76;                // Request controller info for connection handshaking.
+constexpr byte SET_MOTORS_ENABLED_CMD = 0x7A;  // Enable interrupt to control the motors.
 
 // Arduino reboots in around two seconds when a serial connection is initiated.
 // To notify the client that Arduino is ready to receive and transmit data, a ready message
 // is sent.
-byte READY_MSG = 0x80;
+constexpr byte READY_MSG = 0x80;
 
 // Success response code.
-byte SUCCESS_MSG = 0x11;
+constexpr byte SUCCESS_MSG = 0x11;
 
 // Error response code.
-byte ERROR_MSG = 0x12;
+constexpr byte ERROR_MSG = 0x12;
 
 // Last time a message was received.
 long int timeMs = 0;
@@ -75,9 +75,8 @@ long int timeMs = 0;
 byte messageId = 0;
 
 // Stepper motors.
-AccelStepper stepper[] = {
-    AccelStepper{AccelStepper::DRIVER, STEPPER0_STEP_PIN, STEPPER0_DIR_PIN},
-    AccelStepper{AccelStepper::DRIVER, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN}};
+AccelStepper stepper[] = { AccelStepper{ AccelStepper::DRIVER, STEPPER0_STEP_PIN, STEPPER0_DIR_PIN },
+                           AccelStepper{ AccelStepper::DRIVER, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN } };
 
 // Stepper motors configuration: acceleration and max speed.
 // If we make motors run too fast, Arduino will not be able to communicate through
@@ -88,15 +87,11 @@ AccelStepper stepper[] = {
 // To give Arduino time to read and write from and to the serial port, the speed
 // must be reduced further.
 // http://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html
-MotorConfig motorConfig[] = {
-    MotorConfig{1500.0, 1500.0},
-    MotorConfig{1500.0, 1500.0}};
+MotorConfig motorConfig[] = { MotorConfig{ 1500.0, 1500.0 }, MotorConfig{ 1500.0, 1500.0 } };
 
 // This structure holds motor goals: the main thread updates the goal and the
 // ISR reads it.
-MotorGoal motorGoal[] = {
-    MotorGoal{},
-    MotorGoal{}};
+MotorGoal motorGoal[] = { MotorGoal{}, MotorGoal{} };
 
 // This variable tells the ISR not to read a goal while the main thread is
 // inserting a new one.
@@ -104,28 +99,54 @@ volatile bool writingMotorGoals = false;
 
 // This structure holds motor states: the ISR updates the state and the main
 // thread reads it.
-MotorState motorState[] = {
-    MotorState{},
-    MotorState{}};
+MotorState motorState[] = { MotorState{}, MotorState{} };
 
 // This variable tells the ISR not to override the state while the main thread
 // is reading it.
 volatile bool readingMotorStates = false;
 
 // Class to read and write over a serial port.
-SerialPort serialPort{255, 255};
+SerialPort serialPort{ 255, 255 };
 
 // Message to be written to the serial port, usually a response.
-DataBuffer responseBuffer{255};
+DataBuffer responseBuffer{ 255 };
+
+/**
+ * Convert a rotation angle in radians to a number of steps.
+ * @param radians The angle in radians.
+ * @return long The angle in number of steps.
+ */
+long radiansToSteps(float radians)
+{
+  return static_cast<long>(roundf(0.5 * radians * TOTAL_STEPS / PI));
+}
+
+/**
+ * @brief Compute the sign of the given number.
+ * @param val The number to calculate the sign of.
+ * @return The sign of the given number.
+ */
+float sgn(float val)
+{
+  if (val > 0)
+  {
+    return 1.0f;
+  }
+  if (val < 0)
+  {
+    return -1.0f;
+  }
+  return 0.0f;
+}
 
 /**
  * Send a ready message when Arduino is ready to communicate.
  */
 void sendReadyMessage()
 {
-    responseBuffer.addByte(messageId++, Location::END);
-    responseBuffer.addByte(READY_MSG, Location::END);
-    serialPort.write(&responseBuffer);
+  responseBuffer.addByte(messageId++, Location::END);
+  responseBuffer.addByte(READY_MSG, Location::END);
+  serialPort.write(&responseBuffer);
 }
 
 /**
@@ -133,30 +154,29 @@ void sendReadyMessage()
  */
 void returnCommandSuccess(byte requestId)
 {
-    responseBuffer.addByte(requestId, Location::END);
-    responseBuffer.addByte(SUCCESS_MSG, Location::END);
-    serialPort.write(&responseBuffer);
+  responseBuffer.addByte(requestId, Location::END);
+  responseBuffer.addByte(SUCCESS_MSG, Location::END);
+  serialPort.write(&responseBuffer);
 }
 
 /**
- * When a Status command is received, send back the position of the motor and
- * relative speed.
+ * When a Status command is received, send back the position of the motor (rad) and
+ * its speed (rad/s).
  */
 void returnStatus(byte requestId)
 {
-    responseBuffer.addByte(requestId, Location::END);
-    responseBuffer.addByte(SUCCESS_MSG, Location::END);
+  responseBuffer.addByte(requestId, Location::END);
+  responseBuffer.addByte(SUCCESS_MSG, Location::END);
+  {
+    Guard stateGuard{ readingMotorStates };
+    for (int i = 0; i < 2; i++)
     {
-        Guard stateGuard{readingMotorStates};
-        for (int i = 0; i < 2; i++)
-        {
-            responseBuffer.addLong(motorState[i].getPosition(), Location::END);
-            float relativeSpeed = 100.0 * (motorState[i].getSpeed() / motorConfig[i].getMaxSpeed());
-            responseBuffer.addFloat(relativeSpeed, Location::END);
-            responseBuffer.addLong(motorState[i].getDistanceToGo(), Location::END);
-        }
+      responseBuffer.addLong(motorState[i].getPosition(), Location::END);
+      responseBuffer.addFloat(motorState[i].getSpeed(), Location::END);
+      responseBuffer.addLong(motorState[i].getDistanceToGo(), Location::END);
     }
-    serialPort.write(&responseBuffer);
+  }
+  serialPort.write(&responseBuffer);
 }
 
 /**
@@ -165,13 +185,13 @@ void returnStatus(byte requestId)
  */
 void returnControllerInfo(byte requestId)
 {
-    responseBuffer.addByte(requestId, Location::END);
-    responseBuffer.addByte(SUCCESS_MSG, Location::END);
-    for (int i = 0; NAME[i] != '\0'; i++)
-    {
-        responseBuffer.addByte(NAME[i], Location::END);
-    }
-    serialPort.write(&responseBuffer);
+  responseBuffer.addByte(requestId, Location::END);
+  responseBuffer.addByte(SUCCESS_MSG, Location::END);
+  for (int i = 0; NAME[i] != '\0'; i++)
+  {
+    responseBuffer.addByte(NAME[i], Location::END);
+  }
+  serialPort.write(&responseBuffer);
 }
 
 /**
@@ -181,149 +201,94 @@ void returnControllerInfo(byte requestId)
  */
 void setMotorsEnabled(byte requestId, byte enabled)
 {
-    if (enabled == 0)
-    {
-        TimerInterrupt::start(INTERRUPT_TIME_US);
-    }
-    else
-    {
-        TimerInterrupt::stop();
-    }
-    returnCommandSuccess(requestId);
+  if (enabled == 0)
+  {
+    TimerInterrupt::start(INTERRUPT_TIME_US);
+  }
+  else
+  {
+    TimerInterrupt::stop();
+  }
+  returnCommandSuccess(requestId);
 }
 
 /**
- * Set the motor maximum speed.
+ * Move the motors at given speed.
  * @param requestId The id of the request.
- * @param motorId The id of the motor to change the speed.
- * @param speed The speed relative to the maximum speed, from 0 to 100.
+ * @param cmd The move command.
  */
-void setMotorSpeed(byte requestId, byte motorId, float speed)
+void speedCommand(byte requestId, DataBuffer* cmd)
 {
-    if (speed > 100.0)
+  while (cmd->getSize() > 0)
+  {
+    byte motorId = cmd->removeByte(Location::FRONT);
+    float speed = cmd->removeFloat(Location::FRONT);
+
+    float absSpeed = abs(speed);
+    float sgnSpeed = sgn(speed);
+    if (absSpeed > motorConfig[motorId].getMaxSpeed())
     {
-        speed = 100.0;
+      absSpeed = motorConfig[motorId].getMaxSpeed();
     }
-    {
-        Guard goalGuard{writingMotorGoals};
-        motorGoal[motorId].setSpeed(motorConfig[motorId].getMaxSpeed() * speed / 100.0);
-    }
-    returnCommandSuccess(requestId);
+
+    Guard goalGuard{ writingMotorGoals };
+    // Full rotation of a stepper in the direction given by the speed sign.
+    motorGoal[motorId].setPosition(sgnSpeed * TOTAL_STEPS);
+    motorGoal[motorId].setSpeed(absSpeed);
+  }
+  returnCommandSuccess(requestId);
 }
 
 /**
- * Move the motor a given number of steps
+ * Move the motors at maximum speed to a given position.
  * @param requestId The id of the request.
- * @param motorId The id of the motor to change the speed.
- * @param steps The number of steps to move the motor.
+ * @param cmd The move command.
  */
-void moveMotor(byte requestId, byte motorId, long steps)
+void moveCommand(byte requestId, DataBuffer* cmd)
 {
-    {
-        Guard goalGuard{writingMotorGoals};
-        Guard stateGuard{readingMotorStates};
-        // The motor position may not reflect exactly the one stored in the
-        // state while the motor is moving.
-        motorGoal[motorId].setPosition(motorState[motorId].getPosition() + steps);
-    }
+  while (cmd->getSize() > 0)
+  {
+    byte motorId = cmd->removeByte(Location::FRONT);
+    long position = radiansToSteps(cmd->removeFloat(Location::FRONT));
+    float speed = motorConfig[motorId].getMaxSpeed();
 
-    returnCommandSuccess(requestId);
-}
-
-/**
- * Move the motor to a given position.
- * @param requestId The id of the request.
- * @param motorId The id of the motor to change the speed.
- * @param position The position to move the motor.
- */
-void moveMotorTo(byte requestId, byte motorId, long position)
-{
-    {
-        Guard goalGuard{writingMotorGoals};
-        motorGoal[motorId].setPosition(position);
-    }
-    returnCommandSuccess(requestId);
-}
-
-/**
- * Stop all motors.
- * @param requestId The id of the request.
- */
-void stopAllMotors(byte requestId)
-{
-    {
-        Guard goalGuard{writingMotorGoals};
-        for (byte i = 0; i < 2; i++)
-        {
-            motorGoal[i].setSpeed(0);
-        }
-    }
-    returnCommandSuccess(requestId);
-}
-
-/**
- * Stop the given motor.
- * @param requestId The id of the request.
- * @param motorId The id of the motor to stop.
- */
-void stopMotor(byte requestId, byte motorId)
-{
-    {
-        Guard goalGuard{writingMotorGoals};
-        motorGoal[motorId].setSpeed(0);
-    }
-    returnCommandSuccess(requestId);
+    Guard goalGuard{ writingMotorGoals };
+    motorGoal[motorId].setPosition(position);
+    motorGoal[motorId].setSpeed(speed);
+  }
+  returnCommandSuccess(requestId);
 }
 
 /**
  * Decode the input command and execute the requested action.
  * @param msg The input message containing the command.
  */
-void processBuffer(byte requestId, DataBuffer *cmd)
+void processBuffer(byte requestId, DataBuffer* cmd)
 {
-    byte cmdId = cmd->removeByte(Location::FRONT);
+  byte cmdId = cmd->removeByte(Location::FRONT);
 
-    if (cmdId == STATUS_CMD)
-    {
-        returnStatus(requestId);
-    }
-    else if (cmdId == SET_MOTORS_ENABLED_CMD)
-    {
-        byte enabled = cmd->removeByte(Location::FRONT);
-        setMotorsEnabled(requestId, enabled);
-    }
-    else if (cmdId == SET_SPEED_CMD)
-    {
-        byte motorId = cmd->removeByte(Location::FRONT);
-        float targetSpdPercentage = fabs(cmd->removeFloat(Location::FRONT));
-        setMotorSpeed(requestId, motorId, targetSpdPercentage);
-    }
-    else if (cmdId == MOVE_CMD)
-    {
-        byte motorId = cmd->removeByte(Location::FRONT);
-        long steps = cmd->removeLong(Location::FRONT);
-        moveMotor(requestId, motorId, steps);
-    }
-    else if (cmdId == MOVE_TO_CMD)
-    {
-        byte motorId = cmd->removeByte(Location::FRONT);
-        long position = cmd->removeLong(Location::FRONT);
-        moveMotorTo(requestId, motorId, position);
-    }
-    else if (cmdId == STOP_CMD)
-    {
-        byte motorId = cmd->removeByte(Location::FRONT);
-        stopMotor(requestId, motorId);
-    }
-    else if (cmdId == STOP_ALL_CMD)
-    {
-        stopAllMotors(requestId);
-    }
-    else if (cmdId == INFO_CMD)
-    {
-        returnControllerInfo(requestId);
-    }
-    cmd->clear();
+  if (cmdId == STATUS_CMD)
+  {
+    returnStatus(requestId);
+  }
+  else if (cmdId == SET_MOTORS_ENABLED_CMD)
+  {
+    byte enabled = cmd->removeByte(Location::FRONT);
+    setMotorsEnabled(requestId, enabled);
+  }
+  else if (cmdId == MOVE_CMD)
+  {
+    moveCommand(requestId, cmd);
+  }
+  else if (cmdId == SPEED_CMD)
+  {
+    speedCommand(requestId, cmd);
+  }
+  else if (cmdId == INFO_CMD)
+  {
+    returnControllerInfo(requestId);
+  }
+  cmd->clear();
 }
 
 /*
@@ -331,105 +296,105 @@ void processBuffer(byte requestId, DataBuffer *cmd)
  */
 void run()
 {
-    for (int i = 0; i < 2; i++)
+  for (int i = 0; i < 2; i++)
+  {
+    stepper[i].run();
+
+    // Write motor states.
+
+    float actualSpeed = stepper[i].speed();
+    long currentPosition = stepper[i].currentPosition();
+    long distanceToGo = stepper[i].distanceToGo();
+
+    if (!readingMotorStates)
     {
-        stepper[i].run();
-
-        // Write motor states.
-
-        float actualSpeed = stepper[i].speed();
-        long currentPosition = stepper[i].currentPosition();
-        long distanceToGo = stepper[i].distanceToGo();
-
-        if (!readingMotorStates)
-        {
-            motorState[i].setSpeed(actualSpeed);
-            motorState[i].setPosition(currentPosition);
-            motorState[i].setDistanceToGo(distanceToGo);
-        }
-
-        // Read motor goals.
-
-        // AccelStepper library only supports acceleration to maximum speed and
-        // deceleration to 0 speed. The next code introduces support to
-        // decelerate to a given speed.
-
-        if (!writingMotorGoals)
-        {
-            float absSpeed = fabs(actualSpeed);
-            if (motorState[i].isDecelerating())
-            {
-                if (motorGoal[i].getSpeed() > absSpeed)
-                {
-                    stepper[i].setMaxSpeed(motorGoal[i].getSpeed());
-                    stepper[i].moveTo(motorGoal[i].getPosition());
-                    motorState[i].setDecelerating(false);
-                }
-            }
-            else
-            {
-                stepper[i].moveTo(motorGoal[i].getPosition());
-                if (motorGoal[i].getSpeed() > absSpeed)
-                {
-                    stepper[i].setMaxSpeed(motorGoal[i].getSpeed());
-                }
-                else if ((motorGoal[i].getSpeed() < absSpeed))
-                {
-                    motorState[i].setDecelerating(true);
-                    stepper[i].stop();
-                }
-            }
-        }
+      motorState[i].setSpeed(actualSpeed);
+      motorState[i].setPosition(currentPosition);
+      motorState[i].setDistanceToGo(distanceToGo);
     }
+
+    // Read motor goals.
+
+    // AccelStepper library only supports acceleration to maximum speed and
+    // deceleration to 0 speed. The next code introduces support to
+    // decelerate to a given speed.
+
+    if (!writingMotorGoals)
+    {
+      float absSpeed = fabs(actualSpeed);
+      if (motorState[i].isDecelerating())
+      {
+        if (motorGoal[i].getSpeed() > absSpeed)
+        {
+          stepper[i].setMaxSpeed(motorGoal[i].getSpeed());
+          stepper[i].moveTo(motorGoal[i].getPosition());
+          motorState[i].setDecelerating(false);
+        }
+      }
+      else
+      {
+        stepper[i].moveTo(motorGoal[i].getPosition());
+        if (motorGoal[i].getSpeed() > absSpeed)
+        {
+          stepper[i].setMaxSpeed(motorGoal[i].getSpeed());
+        }
+        else if ((motorGoal[i].getSpeed() < absSpeed))
+        {
+          motorState[i].setDecelerating(true);
+          stepper[i].stop();
+        }
+      }
+    }
+  }
 }
 
 void setup()
 {
-    // Initialize serial port.
+  // Initialize serial port.
 
-    serialPort.init(9600);
-    serialPort.setCallback(&processBuffer);
+  serialPort.init(9600);
+  serialPort.setCallback(&processBuffer);
 
-    // Initialize stepper motors.
+  // Initialize stepper motors.
 
-    for (byte i = 0; i < 2; i++)
-    {
-        stepper[i].setMaxSpeed(motorConfig[i].getMaxSpeed());
-        stepper[i].setAcceleration(motorConfig[i].getAcceleration());
-        stepper[0].setCurrentPosition(0);
-        motorGoal[i].setSpeed(motorConfig[i].getMaxSpeed());
-    }
+  for (byte i = 0; i < 2; i++)
+  {
+    stepper[i].setMaxSpeed(motorConfig[i].getMaxSpeed());
+    stepper[i].setAcceleration(motorConfig[i].getAcceleration());
+    stepper[0].setCurrentPosition(0);
+    motorGoal[i].setSpeed(motorConfig[i].getMaxSpeed());
+  }
 
-    // Initialize interrupt.
+  // Initialize interrupt.
 
-    TimerInterrupt::setCallback(&run);
-    TimerInterrupt::start(INTERRUPT_TIME_US);
+  TimerInterrupt::setCallback(&run);
+  TimerInterrupt::start(INTERRUPT_TIME_US);
 
-    // Send a message to the client that Arduino is ready to read/write data.
+  // Send a message to the client that Arduino is ready to read/write data.
 
-    sendReadyMessage();
+  sendReadyMessage();
 
-    timeMs = millis();
+  timeMs = millis();
 }
 
 void loop()
 {
-    serialPort.update(); // Read/write serial port data.
+  serialPort.update();  // Read/write serial port data.
 
-    // This is a safety measure; the client must keep sending messages.
-    // If no command is received within the given timeframe, motors are
-    // stopped.
+  // This is a safety measure; the client must keep sending messages.
+  // If no command is received within the given timeframe, motors are
+  // stopped.
 
-    long newTimeMs = millis();
-    long timeDiffMs = newTimeMs - timeMs;
-    if (timeDiffMs > TIMEOUT_MS)
+  long newTimeMs = millis();
+  long timeDiffMs = newTimeMs - timeMs;
+  if (timeDiffMs > TIMEOUT_MS)
+  {
+    // Stop all motors.
+    Guard goalGuard{ writingMotorGoals };
+    for (byte i = 0; i < 2; i++)
     {
-        // Stop all motors.
-        Guard goalGuard{writingMotorGoals};
-        for (byte i = 0; i < 2; i++)
-        {
-            motorGoal[i].setSpeed(0);
-        }
+      motorGoal[i].setSpeed(0);
     }
-    timeMs = newTimeMs;
+  }
+  timeMs = newTimeMs;
 }
